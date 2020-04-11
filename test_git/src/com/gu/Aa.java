976eff6f33ab1c590a94ce14f1db2cf4ec57653c
Mr.Gu <1563790008@qq.com>
2020-04-11 20:23:55 +0800

    提交test_git工程到本地仓库

---
+++ test_git/src/com/gu/Aa.java
@@ -0,0 +1,9 @@
+package com.gu;
+
+/**
+ * @author Mr.Gu
+ * @date 2020/4/11 20:20
+ * @function :
+ **/
+public class Aa {
+}
---
+++ test_git/src/com/gu/Main.java
@@ -0,0 +1,14 @@
+package com.gu;
+
+/**
+ * @author Mr.Gu
+ * @date 2020/3/31 21:40
+ * @function :  测试将java代码提交到本地版本库
+ **/
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("将java项目提交到本地版本库");
+    }
+
+}
